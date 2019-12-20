1b79d7d03279f472a4ea6d5df5eef988f5b11949
Joel Krauska <jkrauska@gmail.com>
2019-12-19 16:01:52 -0800

    table

--- dump-sched.py
+++ dump-sched.py
@@ -7,10 +7,23 @@ db = dataset.connect('sqlite:///little-league.db')
 schedule = db.get_table('schedule')
 
 import csv
-with open('schedule.csv', 'w', newline='') as csvfile:
-    for slot in schedule.all(order_by=['datestamp']):
-        print('|', end='')
-        for column in slot.values():
-            print(f"{column}|", end='')
-        print('|')
+
+print('|', end='')
+for header in schedule.find_one().keys():
+    print(f"{header}|", end='')
+print()
+
+print('|', end='')
+for header in schedule.find_one().keys():
+    print(f" --- |", end='')
+print()
+
+
+
+
+for slot in schedule.all(order_by=['datestamp']):
+    print('|', end='')
+    for column in slot.values():
+        print(f"{column}|", end='')
+    print()
 
